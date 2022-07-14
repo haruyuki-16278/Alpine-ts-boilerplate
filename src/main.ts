d061d7872176ede5044d43b6613ce56731b65684
haruyuki@homewin <haruyuki.program@gmail.com>
2022-07-15 01:18:42 +0900

    ✨ AlpineJSを追加

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,15 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
+// import './style.css'
+import Alpine from 'alpinejs'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+window.Alpine = Alpine
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+Alpine.store('text', 'Hello World')
+
+Alpine.data('counter', () => ({
+  cnt: 0,
+  onClickButton () {
+    this.cnt++
+  }
+}))
+
+Alpine.start()
