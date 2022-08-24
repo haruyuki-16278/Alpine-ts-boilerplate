9d3fbfa818938d4ec8eb887549c3de10377ade8d
haruyuki@homewin <haruyuki.program@gmail.com>
2022-08-24 21:18:16 +0900

    👍 ディレクトリ構成を修正

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,7 @@
 // 参考: https://github.com/melnikaite/alpine-router
 
-import { init as page1Init } from './page1/page1'
-import { init as page2Init } from './page2/page2'
+import { init as page1Init } from './pages/page1/page1'
+import { init as page2Init } from './pages/page2/page2'
 
 window.addEventListener('hashchange', onRouteChange)
 onRouteChange()
@@ -15,12 +15,12 @@ async function onRouteChange() {
 async function loadRoute(appElement: HTMLElement) {
   switch (window.location.hash) {
     case '#page2':
-      appElement.innerHTML = await fetch('./page2/page2.html').then(template => template.text())
+      appElement.innerHTML = await fetch('./pages/page2/page2.html').then(template => template.text())
       page2Init()
       break
     case '#page1':
     default:
-      appElement.innerHTML = await fetch('./page1/page1.html').then(template => template.text())
+      appElement.innerHTML = await fetch('./pages/page1/page1.html').then(template => template.text())
       page1Init()
       break
   }
